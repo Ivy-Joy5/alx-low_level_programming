bec2e5339c2d9d4b04ce8c0c041e2b7feabc227e
Ivy-Joy5 <Ivy-Joy5>
2023-10-12 17:00:32 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,16 @@ void print_most_numbers(void)
     {
         if (c != '2' && c != '4')
         {
-            _putchar(c);
+            _putchar(c);  // Assuming _putchar is used for character output
         }
     }
 
     _putchar('\n');
+}
+
+int main(void)
+{
+    print_most_numbers();
+    return (0);
 }
 
